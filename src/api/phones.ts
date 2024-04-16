21b292b6dd8f4fd0598df888e33e6547ddef6693
Illia <ironme530@gmail.com>
2024-04-16 12:00:55 +0300

    hotfix 3

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -2,5 +2,5 @@ import { Phone } from '../Types/Phone';
 import { client } from '../utils/fetchClient';
 
 export const getPhones = () => {
-  return client.get<Phone[]>(`/phones.json`);
+  return client.get<Phone[]>(`./phones.json`);
 };
